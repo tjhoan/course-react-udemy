12ffd9d1ceaf8b1c4c21dab9fa51c29d77522b43
tjhoan <tjhoanjose@gmail.com>
2025-01-12 17:04:25 -0500

    :white_check_mark::sparkles: Section 16 | 229 - 236 : Testing AppRouter, Navbar and SeachPage

--- 07-heroes-spa/src/heroes/pages/SearchPage.jsx
+++ 07-heroes-spa/src/heroes/pages/SearchPage.jsx
@@ -30,7 +30,7 @@ export const SearchPage = () => {
         <div className="col-5">
           <h4>Searching</h4>
           <hr />
-          <form onSubmit={onSearchSubmit}>
+          <form onSubmit={onSearchSubmit} aria-label="form">
             <input
               type="text"
               placeholder="Search a hero"
@@ -54,6 +54,7 @@ export const SearchPage = () => {
             Search a hero
           </div>
           <div
+            aria-label="alert-danger"
             className="alert alert-warning animate__animated animate__fadeIn"
             style={{ display: showError ? '' : 'none' }}
           >
